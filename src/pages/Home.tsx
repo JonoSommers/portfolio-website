d55492d1ef2fa2338056a87b080e94aaf3f937f3
Jono Sommers <jonosommers95@gmail.com>
2026-03-19 16:05:44 -0500

    final lint check

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
-    const test = 123
+    const test = 1234
     return (
         <div className="h-screen flex flex-col justify-center items-center text-center">
             <h1 className="text-4xl font-bold">Jono Sommers</h1>
