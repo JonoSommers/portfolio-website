3df64b6ec9bf202bde39e0237ddecfeffed49029
Jono Sommers <jonosommers95@gmail.com>
2026-04-06 09:39:39 -0500

    style contact section

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,26 @@
 export default function Contact() {
     return (
-        <div className="h-screen flex items-center justify-center">
-            <h1>Contact</h1>
-        </div>
+        <section className="min-h-screen bg-zinc-950 text-white px-10 py-24 flex flex-col justify-center font-mono">
+            <div className="h-screen flex flex-col items-center justify-center gap-8">
+                <div className="flex flex-col items-center justify-center">
+                    <h1 className="text-3xl font-semibold text-zinc-100">Contact</h1>
+                    <div className="w-24 h-px bg-emerald-400 mt-1" />  
+                </div>
+                
+                
+                <div className="flex gap-8">
+                    {['GitHub', 'LinedIn', 'Email', 'Twitter'].map((link) => (
+                        <a
+                        key={link}
+                        href={`#${link.toLowerCase().replace(' ', '')}`}
+                        className="text-zinc-400 text-sm tracking-widest uppercase hover:text-emerald-400 transition-colors duration-200"
+                        >
+                        {link}
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </section>
+        
     )
 }
